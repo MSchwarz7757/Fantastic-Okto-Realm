61db9eac01ef01e798901e08ad174b433f872579
Michel <michel_schwarz@ymail.com>
2023-03-08 01:17:27 +0100

    chore: initialize spring boot project

---
+++ src/test/java/com/OktoRealm/OktoRealmGraphQLAPI/OktoRealmGraphQlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OktoRealm.OktoRealmGraphQLAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktoRealmGraphQlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
